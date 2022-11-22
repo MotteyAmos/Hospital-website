8624b04fd917fb7cae58a6803ead582f51c0cb51
Router <akamottey@st.ug.edu.gh>
2022-11-22 15:57:38 +0700

    changes on the navigation

--- index.js
+++ index.js
@@ -6,15 +6,42 @@ const getElements = (element)=>{
     return document.querySelectorAll(element);
 }
 
-//show and hide menu 
+//show and hide menu on a small and medium device
 
-let tooggleMenuIcon = getElement(".tooggle-menu-icon");
+let openIcon = getElement(".open-menu");
+let closeIcon = getElement(".close-menu");
 let container = getElement("header .container");
 
-tooggleMenuIcon.addEventListener("click", ()=>{
-    container.classList.toggle("open");
+if (window.innerWidth <1025){
+    (container.classList.contains(!"close")) &  (container.className += " close");
+    openIcon.addEventListener("click", ()=>{
+        container.className = container.className.replace("close", "open");
+        console.log(container.classList);
+    })
+    
+    closeIcon.addEventListener("click",()=>{
+        container.className = container.className.replace("open", "close");
+        console.log(container.classList);
+    
+    
+    })
+}
 
-})
 
+
+
+
+//active nav link
+let navLinks = getElements(".nav-link");
+
+
+for (let i=0; i<navLinks.length; i++){
+
+    navLinks[i].addEventListener("click", ()=>{
+        var currentPage = getElement(".active");
+        currentPage.className = currentPage.className.replace("active", "");
+        navLinks[i].className += " active";
+    })
+}
 
 
